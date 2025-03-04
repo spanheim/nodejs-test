602ccc179a70472a88c36e15fd08184c76811416
spanheim <marispanheim@gmail.com>
2025-03-04 17:32:51 +0000

    commit with nodejs app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("My greetings frojm application Node.js 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on http://localhost:${PORT}`);
+});
