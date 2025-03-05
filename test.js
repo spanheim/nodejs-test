726c8e494acb5294fd55df44c03db6deb4c0782d
spanheim <marispanheim@gmail.com>
2025-03-05 10:42:47 +0000

    last commit

--- test.js
+++ test.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const request = require("supertest");
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("My greetings frojm application Node.js 🚀");
+    res.send("My greetings from application Node.js 🚀");
 });
 
-app.listen(PORT, () => {
-    console.log(`server started on http://localhost:${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
 });
+
+describe("GET /", function () {
+    it("should return a greeting message", function (done) {
+        request(app)
+            .get("/")
+            .expect(200)
+            .expect("My greetings from application Node.js 🚀", done);
+    });
+});
+
+after(function (done) {
+    server.close(done);
+});
+
